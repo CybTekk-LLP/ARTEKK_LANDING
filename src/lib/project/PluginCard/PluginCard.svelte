b7904a741fecc15d32ec66f69e425a653376fff1
Gourav Saini <gouravsaini9797@gmail.com>
2024-05-06 17:15:24 +0530

    feat: Plugin-Card

--- src/lib/project/PluginCard/PluginCard.svelte
+++ src/lib/project/PluginCard/PluginCard.svelte
@@ -4,18 +4,23 @@
 
 <article>
   <Typography type="caption">Furniture</Typography>
+  <br />
   <img height="196px" src="https://picsum.photos/200/300" alt="" />
+  <br />
+  <br />
   <button>Configure</button>
 </article>
 
 <style lang="scss">
   article {
     background-color: var(--secondary-500);
-    padding: 10px;
+    padding: 20px;
+    border-radius: 16px;
     & > img {
       inline-size: 100%;
       object-fit: cover;
       image-rendering: optimizeSpeed;
+      border-radius: 6px;
     }
   }
 </style>
