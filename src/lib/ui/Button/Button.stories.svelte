df17c7af55f78b70be3f510a32d1b73316bb1b4c
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-06 17:57:14 +0530

    feat/button

---
+++ src/lib/ui/Button/Button.stories.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+    import { Template, Story } from "@storybook/addon-svelte-csf";
+    import { Button } from "..";
+  
+    export const meta = {
+      title: "Components/UI/Button",
+      component: Button
+    };
+  </script>
+  
+  <Template let:args>
+    <Button {...args} />
+  </Template>
+  
+  <Story name="primary" args={{type: "primary" ,buttonLabel : "GETTING STARTED"}} />
+  <Story name="secondary" args={{type: "secondary",buttonLabel : "CONFIGURE"}} />
+  
