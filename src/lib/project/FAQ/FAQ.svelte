9b281b46eeca5be9d6edc40d4877bcd952fe4bb1
LovelySehotra12 <l.sehotra@cybtekk.com>
2024-05-07 12:28:06 +0530

    feat/faq

---
+++ src/lib/project/FAQ/FAQ.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import Typography from "$lib/ui/Typography/Typography.svelte";
+  export let content: any;
+</script>
+
+{#each content ?? [] as faq, i}
+  <details>
+    <summary>
+      <Typography type="impact">{faq.title}</Typography>
+    </summary>
+    <p>
+      <Typography type="impact" _color="var(--secondary-500)"
+        >{faq.description}</Typography
+      >
+    </p>
+  </details>
+{/each}
+
+<style lang="scss">
+  details {
+    position: relative;
+    margin-inline: 1rem;
+    border: 1px solid transparent;
+    &[open] {
+      padding-bottom: 1em;
+      border: 1px solid var(--primary-300);
+      &::after {
+        content: url("/images/FAQ/minus.svg");
+        position: absolute;
+        inset-inline-end: 15px;
+        inset-block-start: 15px;
+        pointer-events: none;
+      }
+      & > summary {
+        border: 1px solid var(--transparent);
+      }
+    }
+    &::after {
+      content: url("/images/FAQ/plus.svg");
+      position: absolute;
+      inset-inline-end: 15px;
+      inset-block-start: 15px;
+      pointer-events: none;
+    }
+    & > summary::before,
+    & > summary::after {
+      -webkit-transition: 200ms;
+      -o-transition: 200ms;
+      -moz-transition: 200ms;
+      transition: 200ms;
+    }
+    & > summary {
+      padding-block: 12px;
+      padding-inline: 16px;
+      border: 1px solid var(--secondary-500);
+      cursor: pointer;
+      padding-inline-end: 50px;
+      &::-webkit-details-marker {
+        display: none;
+      }
+    }
+    & > summary::marker {
+      content: none;
+    }
+    & > p {
+      padding-inline: 1em;
+    }
+  }
+</style>
--- src/lib/project/index.ts
+++ src/lib/project/index.ts
@@ -1,2 +1,3 @@
 export { default as AboutCard } from "./AboutCard/AboutCard.svelte";
 export { default as PluginCard } from "./PluginCard/PluginCard.svelte";
+export { default as FAQ } from "./FAQ/FAQ.svelte";
