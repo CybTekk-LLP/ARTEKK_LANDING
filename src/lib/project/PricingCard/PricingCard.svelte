0c8fa2b15e05ed06c7f8076242e818e83efddb73
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-07 11:25:12 +0530

    feat/pricing-card

---
+++ src/lib/project/PricingCard/PricingCard.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { Typography } from "$lib/ui";
+  import Button from "$lib/ui/Button/Button.svelte";
+  export let planLabel: string;
+  export let amount: string;
+  export let description: string;
+  export let onClick = () => {};
+</script>
+
+<article>
+  <div class="pricing">
+    <Typography type="body">{planLabel}</Typography>
+    <Typography type="heading">{amount}</Typography>
+
+    <ul>
+      <Typography type="impact"><li>{description}</li></Typography>
+    </ul>
+
+    <Button type="secondary" buttonLabel="CONFIGURE" {onClick}></Button>
+  </div>
+</article>
+
+<style lang="scss">
+  article {
+    background: var(--secondary-600);
+    border-radius: 16px;
+
+    & > .pricing {
+      padding: 20px;
+      inline-size: 90%;
+      margin-inline: auto;
+      display: grid;
+      gap: 15px;
+    }
+    li {
+      list-style-image: url('data:image/svg+xml,<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.52142 8.31994L6.5083 11.1999L12.4814 5.43994" stroke="%23D9D9D9" stroke-linecap="round" stroke-linejoin="round"/></svg>');
+      list-style-position: inside;
+    }
+  }
+</style>
--- src/lib/project/index.ts
+++ src/lib/project/index.ts
@@ -1,2 +1,3 @@
 export { default as AboutCard } from "./AboutCard/AboutCard.svelte";
 export { default as PluginCard } from "./PluginCard/PluginCard.svelte";
+export { default as PricingCard } from "./PricingCard/PricingCard.svelte";
