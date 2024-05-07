4a6f13044ddb990d40411d29a434d83d3aface72
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-07 13:03:27 +0530

    fix/css

---
+++ src/lib/ui/InputText/InputText.stories.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import { InputText } from "..";
+
+  export const meta = {
+    title: "Components/UI/InputText",
+    component: InputText,
+  };
+</script>
+
+<Template let:args>
+  <InputText {...args} />
+</Template>
+
+<Story name="main" args={{ variant: "text", placeholder: "Name" }} />
+<Story name="email" args={{ variant: "email", placeholder: "Email" }} />
