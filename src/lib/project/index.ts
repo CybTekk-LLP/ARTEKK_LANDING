1873d3a8e1552866a07e65ec69c1e1c36a3d8c68
Gourav Saini <gouravsaini9797@gmail.com>
2024-05-07 17:29:14 +0530

    feat: modal

--- src/lib/project/index.ts
+++ src/lib/project/index.ts
@@ -2,3 +2,4 @@ export { default as AboutCard } from "./AboutCard/AboutCard.svelte";
 export { default as PluginCard } from "./PluginCard/PluginCard.svelte";
 export { default as FAQ } from "./FAQ/FAQ.svelte";
 export { default as PricingCard } from "./PricingCard/PricingCard.svelte";
+export { default as Modal } from "./Modal/Modal.svelte";
