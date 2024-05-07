01285a8b1677665e4a5b290e4441efcee28745fc
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-07 13:19:08 +0530

    fix/css

---
+++ src/lib/ui/TextArea/TextArea.stories.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import { TextArea } from "..";
+
+  export const meta = {
+    title: "Components/UI/TextArea",
+    component: TextArea,
+  };
+</script>
+
+<Template let:args>
+  <TextArea {...args} />
+</Template>
+
+<Story name="main" args={{ placeholder: "Description" }} />
---
+++ src/lib/ui/TextArea/TextArea.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let placeholder: string;
+  export let value: string | undefined;
+  export let uniqueId = "inputText" + Math.random().toString().split(".")[1];
+</script>
+
+<textarea id={uniqueId} {placeholder} bind:value />
+
+<style lang="scss">
+  textarea {
+    inline-size: 100%;
+    block-size: 220px;
+    padding: 20px;
+    background-color: var(--secondary-700);
+    border: none;
+    border-radius: 6px;
+    font-size: 16px;
+    color: #828282;
+    font-family: var(--font);
+    font-weight: 400;
+    outline: var(--transparent);
+  }
+</style>
