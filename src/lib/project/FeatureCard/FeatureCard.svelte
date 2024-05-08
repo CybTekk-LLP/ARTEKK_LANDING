96293968dafa883328175bf5c6416ca69cd017b3
yashrajbharti <y.bharti@cybtekk.com>
2024-05-08 14:50:34 +0530

    fix/ featureCard

---
+++ src/lib/project/FeatureCard/FeatureCard.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    let direction = "left";
+</script>
+
+<div>
+    <img src="./images/FeatureCard/Cars.webp" alt="" height="280px" />
+    <img src="./images/FeatureCard/Cars.webp" alt="" height="280px" />
+</div>
+
+<style lang="scss">
+
+    div{
+        position: relative;
+        &> img{
+            &:last-of-type{
+                position: absolute;
+                inset-inline-start: 0;
+                
+            }
+            &:first-of-type{
+                rotate: 8deg;
+                animation: shuffle 1.3s ease;
+            }
+            @keyframes shuffle {
+                0%{
+                    rotate: 8deg;
+                }
+                10%{
+                    rotate: -4deg;
+                }
+                40%{
+                    rotate: 8deg;
+                }
+                60%{
+                    rotate: 0deg;
+                }    
+                100%{
+                    rotate: 8deg;
+                }
+            }
+        }
+    }
+</style>
--- src/lib/project/index.ts
+++ src/lib/project/index.ts
@@ -3,3 +3,4 @@ export { default as PluginCard } from "./PluginCard/PluginCard.svelte";
 export { default as FAQ } from "./FAQ/FAQ.svelte";
 export { default as PricingCard } from "./PricingCard/PricingCard.svelte";
 export { default as Modal } from "./Modal/Modal.svelte";
+export { default as FeatureCard } from "./FeatureCard/FeatureCard.svelte"
