da29db56b47f1b8de487a8868ac21d2c123171a1
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-05-20 12:21:23 +0530

    add AR code

--- src/routes/(ar)/footwear/+page.svelte
+++ src/routes/(ar)/footwear/+page.svelte
@@ -7,9 +7,10 @@
         "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzEzODAzNTY4LCJzdWIiOiIwZjBjYWY5Mi1hYjMyLTQyZWEtYjgwZC05ZDQ5N2VlOTYwMTV-U1RBR0lOR35kOTBiZGJkMS03MzU0LTRjNTYtYTYwMi1jMGEyMjJlNmJmYmYifQ.uDZfZL0qjbTggO8K1KWGLmIgYSc_n9A6fgC5-8zD0QQ",
     });
     const liveRenderTarget = document.getElementById(
-      "stream",
+      "stream"
     ) as HTMLCanvasElement;
     const session = await cameraKit.createSession({ liveRenderTarget });
+
     const mediaStream = await navigator.mediaDevices.getUserMedia({
       video: {
         facingMode: { exact: "environment" },
@@ -20,7 +21,7 @@
 
     const lens = await cameraKit.lensRepository.loadLens(
       "324ffc1d-1407-45ce-b2c5-1fad38de7d18",
-      "88100c29-c3e6-4deb-bce0-35e91383790e",
+      "88100c29-c3e6-4deb-bce0-35e91383790e"
     );
     await session.applyLens(lens);
   })();
