91e3ba2585d24fc89cebc6548ef6721e0cdac320
LovelySehotra12 <l.sehotra@cybtekk.com>
2024-05-22 12:04:49 +0530

    refactor/contactsales

---
+++ src/routes/(landing)/getstarted/+page.svelte
@@ -0,0 +1,31 @@
+<!-- <script lang="ts">
+    import { FeatureCard } from "$lib/project";
+    import { onMount } from "svelte";
+    import { Navbar, Typography } from "$lib/ui";
+</script>
+<div class="started-page">
+
+
+    <h1 class="heading">
+      <Typography type="heading"
+        >“Experience Reality Like Never Before”</Typography
+      >
+    </h1>
+    <p class="description">
+      <Typography type="body" _color="var(--primary-700)">
+        At ARTEKK, we redefine what's possible with Augmented Reality (AR).
+        Explore immersive experiences, engage your audience, and unlock new
+        dimensions of creativity with our cutting-edge AR solutions.
+      </Typography>
+    </p>
+    <button class="getstarted">Get Started</button>
+  </div>
+
+
+
+<style lane="scss">
+.started-page{
+    background-image:url(/images/Home/BannerOne.png);
+    background-repeat:no-repeat;
+}
+</style> -->
