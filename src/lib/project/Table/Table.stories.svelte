bf09fcc31c425dd7a6a4b335b65ac21e189bb982
Gourav Saini <gouravsaini9797@gmail.com>
2024-05-22 10:47:37 +0530

    feat: table

---
+++ src/lib/project/Table/Table.stories.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import { Table } from "..";
+
+  export const meta = {
+    title: "Components/Project/Table",
+    component: Table,
+  };
+</script>
+
+<Template let:args>
+  <Table {...args} />
+</Template>
+
+<Story name="main" args={{}} />
---
+++ src/lib/project/Table/Table.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Typography } from "./../../ui";
+</script>
+
+<table>
+  <thead>
+    <th
+      ><Typography type="subtext" _color="var(--primary-700)"
+        >Project name</Typography
+      ></th
+    >
+    <th
+      ><Typography type="subtext" _color="var(--primary-700)">Domain</Typography
+      ></th
+    >
+    <th
+      ><Typography type="subtext" _color="var(--primary-700)">Status</Typography
+      ></th
+    >
+    <th
+      ><Typography type="subtext" _color="var(--primary-700)">Action</Typography
+      ></th
+    >
+  </thead>
+  <tbody>
+    <tr>
+      <td><Typography type="subtext">Amazon</Typography></td>
+      <td><Typography type="subtext">https://www.amazon.in/</Typography></td>
+      <td><slot name="status" /></td>
+      <td><slot name="actions" /></td>
+    </tr>
+  </tbody>
+</table>
