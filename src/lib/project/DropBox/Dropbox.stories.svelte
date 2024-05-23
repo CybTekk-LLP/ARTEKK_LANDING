2136260d1e8e3ccf426ddce555ecb5bf06e29d85
Gourav Saini <gouravsaini9797@gmail.com>
2024-05-23 10:56:11 +0530

    chore: added new color

---
+++ src/lib/project/DropBox/Dropbox.stories.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import { DropBox } from "..";
+
+  export const meta = {
+    title: "Components/Project/DropBox",
+    component: DropBox,
+  };
+</script>
+
+<Template let:args>
+  <DropBox {...args} />
+</Template>
+
+<Story name="main" args={{}} />
