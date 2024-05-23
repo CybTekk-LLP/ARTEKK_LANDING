9cf420c0ac31d2f6020a0199623b85c4b9486384
yashrajbharti <y.bharti@cybtekk.com>
2024-05-23 10:34:51 +0530

    highlight js

---
+++ src/lib/project/CodeSnippet/CodeSnippet.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import hljs from "highlight.js";
+    import "highlight.js/styles/atom-one-dark.css"; // You can choose any style you like
+
+    export let code: string = "";
+    export let language: string | null = null;
+    let codeElement: HTMLElement;
+    let highlightedCode: string = "";
+
+    onMount(() => {
+        if (language) {
+            highlightedCode = hljs.highlight(language, code).value;
+        } else {
+            highlightedCode = hljs.highlightAuto(code).value;
+        }
+    });
+</script>
+
+<pre>
+    <code class="hljs" bind:this={codeElement}>{@html highlightedCode}</code>
+  </pre>
+
+<style lang="scss">
+    pre {
+        & > code {
+            border-radius: 4px;
+        }
+    }
+</style>
