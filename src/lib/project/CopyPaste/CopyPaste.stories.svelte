c941ea3e050bfc7b9904892113c4577f8c7ac7da
yashrajbharti <y.bharti@cybtekk.com>
2024-05-23 11:03:26 +0530

    fix

---
+++ src/lib/project/CopyPaste/CopyPaste.stories.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+    import { Story, Template } from "@storybook/addon-svelte-csf";
+    import { CopyPaste } from "..";
+
+    export const meta = {
+        title: "Components/Project/CopyPaste",
+        component: CopyPaste,
+    };
+</script>
+
+<Template let:args>
+    <CopyPaste {...args} text="Copied to Clipboard"/>
+</Template>
+
+<Story name="Main" />
