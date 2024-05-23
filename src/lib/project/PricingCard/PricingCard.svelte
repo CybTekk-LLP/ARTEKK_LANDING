7c8cea30263bc6f6d120995f21afe28f3c0dca52
LovelySehotra12 <l.sehotra@cybtekk.com>
2024-05-23 10:57:48 +0530

    remove unuse code

--- src/lib/project/PricingCard/PricingCard.svelte
+++ src/lib/project/PricingCard/PricingCard.svelte
@@ -42,8 +42,6 @@
     }
   }
   li {
-    // list-style-image: url('data:image/svg+xml,<svg width="16" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.521423 4.31994L3.5083 7.19994L9.48142 1.43994" stroke="%23D9D9D9" stroke-linecap="round" stroke-linejoin="round"/></svg>');
-    // list-style-position: inside;
     list-style: none;
     display: flex;
     gap: 10px;
