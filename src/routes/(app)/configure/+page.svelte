99bc59aa8b634fd2b3b6d785db67884932d28b5b
LovelySehotra12 <l.sehotra@cybtekk.com>
2024-05-23 14:23:41 +0530

    feat/activepage

--- src/routes/(app)/configure/+page.svelte
+++ src/routes/(app)/configure/+page.svelte
@@ -2,13 +2,29 @@
   import { Status, Typography, InputText, Button } from "$lib/ui";
 
   let projectName: string;
+  let status: "active" | "inActive";
 </script>
 
 <main>
   <form>
     <Typography type="cardTitle">Configure Your Project</Typography>
     <br /><br />
-    <Status type="inActive" statusLabel="Inactive" />
+    <div class="status">
+      <div class="tool-label">
+        <Typography type="subtext" _fontweight="400"
+          >Use our <Typography
+            type="subtext"
+            _fontweight="400"
+            _color="var(--tertiary-700)"
+            renderInline>Generator</Typography
+          > Tool for the Code</Typography
+        >
+      </div>
+      <Status
+        type={status !== "active" ? "inActive" : "active"}
+        statusLabel={status !== "active" ? "InActive" : "active"}
+      />
+    </div>
     <br />
     <br />
     <InputText
@@ -57,9 +73,22 @@
       max-inline-size: 550px;
       inline-size: 90vw;
       margin-inline: auto;
+      & > .status {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        & > .tool-label {
+          border: 1px solid var(--secondary-500);
+          border-radius: 10px;
+          padding-inline: 16px;
+          padding-block: 8px;
+        }
+      }
       & > .domain-label {
         margin-block-end: 5px;
       }
+
       & > .domains {
         margin-block-end: 10px;
       }
