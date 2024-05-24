f7ba4fe89c9ed8df53dfed578ff61d088b030adf
LovelySehotra12 <l.sehotra@cybtekk.com>
2024-05-24 17:41:41 +0530

    add suggestionName

---
+++ src/lib/project/Suggestion/Suggestion.stories.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import { Suggestion } from "..";
+
+  export const meta = {
+    title: "Components/Project/Suggestion",
+    component: Suggestion,
+  };
+</script>
+
+<Template let:args>
+  <Suggestion {...args} />
+</Template>
+
+<Story name="main" args={{ value: 60 }} />
