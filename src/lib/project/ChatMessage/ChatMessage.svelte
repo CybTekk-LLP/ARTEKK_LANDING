87b91ddb3ca95d8aa5ddc8016704602b1badcf38
yashrajbharti <y.bharti@cybtekk.com>
2024-07-01 11:45:59 +0530

    fix

--- src/lib/project/ChatMessage/ChatMessage.svelte
+++ src/lib/project/ChatMessage/ChatMessage.svelte
@@ -10,6 +10,7 @@
   class="chat-message"
   class:messageUser={messageType === "user"}
   id={"chat-" + index}
+  data-message={messageType}
 >
   <p class="message" class:message-heading-user={messageType === "user"}>
     <Typography type="subtext">{message}</Typography>
@@ -24,6 +25,7 @@
     inline-size: 100%;
     display: grid;
     margin-block: 8px;
+    position: relative;
 
     & > .message {
       background-color: var(--secondary-700);
@@ -40,8 +42,18 @@
     & > .chat-time {
       text-align: end;
     }
+    &[data-message="user"]:last-of-type::after{
+      position: absolute;
+      content: url("data:image/svg+xml,%3Csvg width='50' height='6.75' viewBox='0 0 120 30' xmlns='http://www.w3.org/2000/svg' fill='%234c32b2'%3E%3Ccircle cx='15' cy='15' r='15'%3E%3Canimate attributeName='r' from='15' to='9' begin='0s' dur='0.8s' values='15;9;15' calcMode='linear' repeatCount='indefinite' /%3E%3Canimate attributeName='fill-opacity' from='1' to='0.5' begin='0s' dur='0.8s' values='1;0.5;1' calcMode='linear' repeatCount='indefinite' /%3E%3C/circle%3E%3Ccircle cx='60' cy='15' r='9' fill-opacity='0.3'%3E%3Canimate attributeName='r' from='9' to='15' begin='0s' dur='0.8s' values='9;15;9' calcMode='linear' repeatCount='indefinite' /%3E%3Canimate attributeName='fill-opacity' from='0.5' to='1' begin='0s' dur='0.8s' values='0.5;1;0.5' calcMode='linear' repeatCount='indefinite' /%3E%3C/circle%3E%3Ccircle cx='105' cy='15' r='15'%3E%3Canimate attributeName='r' from='15' to='9' begin='0s' dur='0.8s' values='15;9;15' calcMode='linear' repeatCount='indefinite' /%3E%3Canimate attributeName='fill-opacity' from='1' to='0.5' begin='0s' dur='0.8s' values='1;0.5;1' calcMode='linear' repeatCount='indefinite' /%3E%3C/circle%3E%3C/svg%3E%0A");
+      background-color: var(--secondary-700);
+      border-radius: 10px;
+      padding: 5px 10px 10px 10px;
+      inset-block-start: 20px;
+      inset-inline-start: 10px;
+    }
   }
   .messageUser {
     justify-content: end;
   }
+
 </style>
