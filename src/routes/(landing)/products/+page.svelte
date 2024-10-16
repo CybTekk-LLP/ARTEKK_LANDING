3a7c922ad962907c2d23d60de981ea27f6f2bfab
Gourav Saini <gouravsaini9797@gmail.com>
2024-10-16 16:34:16 +0530

    fix: content

--- src/routes/(landing)/products/+page.svelte
+++ src/routes/(landing)/products/+page.svelte
@@ -23,30 +23,78 @@
     }
   };
   export let pluginContent = [
-    { cardHeading: "Furniture", subHeading: "Furniture AR lets you visualize furniture in your space through AR before purchase.", imgSrc: "./images/Collections/Furniture.webp" },
-    { cardHeading: "Footwear", subHeading: "Footwear AR lets you virtually try on and visualize shoes in real-time before purchase.", imgSrc: "./images/Collections/Shoes.webp" },
-    { cardHeading: "Clothing", subHeading:"Clothing AR lets you virtually try on outfits in real-time before purchase.", imgSrc: "./images/Collections/Clothes.webp" },
-    { cardHeading: "Watches", subHeading: "Watches AR lets you virtually try on timepieces in real-time before purchase.", imgSrc: "./images/Collections/Watch.webp" },
-    { cardHeading: "Gloves", subHeading: "Gloves AR lets you virtually try on gloves in real-time before purchase.", imgSrc: "./images/Collections/Gloves.webp" },
-    { cardHeading: "Jewellery", subHeading: "Jewellery AR enables you to virtually try on pieces in real-time before purchase.", imgSrc: "./images/Collections/Jewellery.webp" },
-    { cardHeading: "Cap", subHeading: "Caps AR lets you virtually try on headwear in real-time before purchase.", imgSrc: "./images/Collections/Caps.webp" },
-    { cardHeading: "Cars",subHeading: "Cars AR allows you to explore and visualize vehicles in real-time before purchase.",imgSrc: "./images/Collections/Cars.webp" },
+    {
+      cardHeading: "Furniture",
+      subHeading:
+        "Enable clients to visualize furniture in their space through augmented reality before purchase, enhancing decision-making and reducing returns.",
+      imgSrc: "./images/Collections/Furniture.webp",
+    },
+    {
+      cardHeading: "Footwear",
+      subHeading:
+        "Allow clients to virtually try on and visualize shoes in real-time before purchase, enhancing their shopping experience.",
+      imgSrc: "./images/Collections/Shoes.webp",
+    },
+    {
+      cardHeading: "Clothing",
+      subHeading:
+        "Enable clients to virtually try on outfits in real-time before purchase, offering a more immersive and personalized shopping experience.",
+      imgSrc: "./images/Collections/Clothes.webp",
+    },
+    {
+      cardHeading: "Watches",
+      subHeading:
+        "Allow clients to virtually try on timepieces in real-time before purchase, enhancing their shopping experience with a personalized touch.",
+      imgSrc: "./images/Collections/Watch.webp",
+    },
+    {
+      cardHeading: "Gloves",
+      subHeading:
+        "Enable clients to virtually try on gloves in real-time before purchase, offering a more interactive and personalized shopping experience.",
+      imgSrc: "./images/Collections/Gloves.webp",
+    },
+    {
+      cardHeading: "Jewellery",
+      subHeading:
+        "Let clients virtually try on jewellery in real-time before purchase, providing a luxurious and engaging shopping experience.",
+      imgSrc: "./images/Collections/Jewellery.webp",
+    },
+    {
+      cardHeading: "Cap",
+      subHeading:
+        "Allow clients to virtually try on headwear in real-time before purchase, enhancing their shopping experience with personalized fitting.",
+      imgSrc: "./images/Collections/Caps.webp",
+    },
+    {
+      cardHeading: "Cars",
+      subHeading:
+        "Enable clients to explore and visualize vehicles in real-time before purchase, offering a dynamic and immersive buying experience.",
+      imgSrc: "./images/Collections/Cars.webp",
+    },
     {
       cardHeading: "Newspaper",
-      subHeading: "Newspaper AR enables you to visualize content in an interactive way before release.",
+      subHeading:
+        "Enable clients to visualize and interact with newspaper content before release, offering a more engaging and dynamic preview experience.",
       imgSrc: "./images/Collections/Newspapers.webp",
     },
     {
       cardHeading: "Billboard",
-      subHeading:  "Billboard AR lets you visualize ads on real-world displays before launch.",
+      subHeading:
+        "Allow clients to visualize ads on real-world displays before launch, offering a more precise and impactful advertising experience.",
       imgSrc: "./images/Collections/Billboards.webp",
     },
     {
       cardHeading: "Makeup",
-      subHeading: "Makeup AR enables you to try on products virtually in real-time before buying.",
+      subHeading:
+        "Enable clients to virtually try on makeup products in real-time before purchasing, enhancing their shopping experience with interactive and personalized trials.",
       imgSrc: "./images/Collections/Makeup.webp",
     },
-    { cardHeading: "Glasses", subHeading: "Glasses AR lets you virtually try on eyewear in real-time before purchase.", imgSrc: "./images/Collections/Glasses.webp" },
+    {
+      cardHeading: "Glasses",
+      subHeading:
+        "Allow clients to virtually try on eyewear in real-time before purchase, providing an engaging and personalized shopping experience.",
+      imgSrc: "./images/Collections/Glasses.webp",
+    },
   ];
 </script>
 
